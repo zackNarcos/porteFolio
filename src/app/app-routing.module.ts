c19d7f74a43064541103a3273898b3c3c6786221
zack abessolo <zackabess@gmail.com>
2023-11-13 13:35:39 +0000

    update 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,11 @@ const routes: Routes = [
         path: 'home',
         loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
       },
+      {
+        path: '**',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      }
     ]
   }
 ];
